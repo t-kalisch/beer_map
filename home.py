1995d9adc71b58aa7d16a1170f0f2bdb56e55a31
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2024-05-25 23:01:03 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,17 +1,15 @@
 import streamlit as st
-import pandas as pd
+
 from streamlit_gsheets import GSheetsConnection
 
+# Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read(
-    worksheet="coord",
-    usecols=[0, 1],
-)
-print(df)
-st.write(df)
-for row in df.itertuples():
-  st.write(f"{row.lat} has a :{row.long}:")
 
+df = conn.read()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
 st.write("Hello")
 
 st.map()

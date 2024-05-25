2bfea257c98f5907f1447bb633f66959405364c3
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2024-05-25 23:43:07 +0200

    Update home.py

--- home.py
+++ home.py
@@ -27,9 +27,9 @@ def check_in(street, city, country):
   lon = location.longitude
   st.write(lat, lon)
 
-  coords ={"lat": lat, "lon": lon}
-  st.write(coords)
-  new = pd.DataFrame(columns=df.columns, data=coords)
+  new = [lat, lon}
+  
+  new = pd.DataFrame(columns=df.columns, data=[new])
   # Overwrite original dataframe
   df = pd.concat([df, new], axis=0)
   conn.update(data=ndf)

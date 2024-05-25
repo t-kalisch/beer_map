03521ad364e57c1f2aea290af6bf4a8759bad018
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2024-05-25 23:04:46 +0200

    Update home.py

--- home.py
+++ home.py
@@ -4,7 +4,8 @@ from streamlit_gsheets import GSheetsConnection
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-
+st.write(conn)
+st.help(conn)
 df = conn.read()
 st.markdown(df)
 # Print results.

1e16b272c9e903cfe1c447e0f9c0d6e036a8d18e
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2024-05-25 22:59:19 +0200

    Update home.py

--- home.py
+++ home.py
@@ -3,7 +3,10 @@ import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read()
+df = conn.read(
+    worksheet="coord",
+    usecols=[0, 1],
+)
 print(df)
 st.write(df)
 for row in df.itertuples():

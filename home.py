3ac3f3911b70e965949ee6b6e07b1283428c30fd
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2024-05-25 22:57:08 +0200

    Update home.py

--- home.py
+++ home.py
@@ -4,6 +4,7 @@ from streamlit_gsheets import GSheetsConnection
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read()
+print(df)
 st.write(df)
 for row in df.itertuples():
   st.write(f"{row.lat} has a :{row.long}:")

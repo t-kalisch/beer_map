f3522a0c7fbb8d7230131604638fc98e9c982806
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2024-05-25 22:52:28 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read()
+
+for row in df.itertuples():
+  st.write(f"{row.name} has a :{row.pet}:")
 
 st.write("Hello")
 

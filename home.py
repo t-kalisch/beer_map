1b6d3a738d183e0f09d3de1e9b203af287942ea4
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2024-05-25 23:16:44 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,12 +1,37 @@
 import streamlit as st
-
+import geopandas as gpd
+import geopy
+import pandas as pd
 from streamlit_gsheets import GSheetsConnection
+
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read(ttl=0)
 st.write(df)
-# Print results.
+# Print results
 
 st.map(data = df)
+
+
+
+
+street = st.sidebar.text_input("Street", "75 Bay Street")
+city = st.sidebar.text_input("City", "Toronto")
+province = st.sidebar.text_input("Province", "Ontario")
+country = st.sidebar.text_input("Country", "Canada")
+
+geolocator = Nominatim(user_agent="GTA Lookup")
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+location = geolocator.geocode(street+", "+city+", "+province+", "+country)
+
+lat = location.latitude
+lon = location.longitude
+
+map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
+
+st.map(map_data) 

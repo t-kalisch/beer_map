1f22cf705585ee9c01809cdda1ec4912f4ad56ec
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2024-05-28 23:19:11 +0200

    Update home.py

--- home.py
+++ home.py
@@ -13,7 +13,7 @@ from geopy.extra.rate_limiter import RateLimiter
 # Create a connection object.
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 st.write(conn)
-df = conn.read(worksheet="coord",ttl="10m")
+df = conn.read(ttl="10m")
 st.write(df)
 # Print results
 st.map(data = df)

21674fd63ed37ee56ddeee3d9f7cb20dab0df8da
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2024-05-31 13:09:21 +0200

    Update home.py

--- home.py
+++ home.py
@@ -33,8 +33,8 @@ for i in range(0,len(df)):
       radius=100*float(df.iloc[i]['size']),
       color='#000000',
       fill=True,
-      opacity=0.1,
-      fill_opacity=0.1,
+      opacity=1,
+      fill_opacity=0.6,
       fill_color='#fcdb6f',
       
    ).add_to(n)
